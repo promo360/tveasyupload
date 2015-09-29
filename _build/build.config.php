7596006fcfe35ed7e1ea9b8ec0af52124907ec1e
promo360 <info@promo360.ru>
2015-09-29 17:32:07 +0500

    v.1.0.0.beta2

--- _build/build.config.php
+++ _build/build.config.php
@@ -3,7 +3,7 @@
     define('PKG_NAME', 'FastUploadTV');
     define('PKG_NAME_LOWER',str_replace(' ','',strtolower(PKG_NAME)));
     define('PKG_VERSION','1.0.0');
-    define('PKG_RELEASE','beta');
+    define('PKG_RELEASE','beta2');
 
 
 
