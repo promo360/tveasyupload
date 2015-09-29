bdd6fcd52ceafb6bb69a768a9ec6dae945bdc30c
promo360 <info@promo360.ru>
2015-09-29 16:58:03 +0500

    Add settings translit

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -68,6 +68,22 @@
 
     // Build transport vehicle -------------------------------------------------------------
     $builder->putVehicle($vehicle);
+    
+    
+    // Settings ----------------------------------------------------------------------------
+    $settings = include_once $sources['data'].'transport.settings.php';
+    $attributes= array(
+        xPDOTransport::UNIQUE_KEY => 'key',
+        xPDOTransport::PRESERVE_KEYS => true,
+        xPDOTransport::UPDATE_OBJECT => false,
+    );
+    if (!is_array($settings)) { $modx->log(modX::LOG_LEVEL_FATAL,'Adding settings failed.'); }
+    foreach ($settings as $setting) {
+        $vehicle = $builder->createVehicle($setting,$attributes);
+        $builder->putVehicle($vehicle);
+    }
+    $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($settings).' system settings.'); flush();
+    unset($settings,$setting,$attributes);
 
 
     // Adding in docs ----------------------------------------------------------------------
